10d42f2e2b8ce0ee8533aefe5806686958b4635a
Maxwell C Baldridge <max.baldridge@gmail.com>
2017-02-14 14:38:17 -0600

    added event listeners to the things in the index

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,3 +1,50 @@
 "use strict";
 
-let $ = require("jquery");
+// let $ = require('jquery');
+
+///////////////////////////////////////////////
+/////////// EVENT LISTENERS ///////////////////
+///////////////////////////////////////////////
+console.log("We're listening!");
+
+/////////// USER SIGN-IN BUTTON ///////////////
+$("#signIn").click(function() {
+	console.log("sign in user");
+});
+
+$("#signOut").click(function(){
+	location.reload();
+});
+
+/////////// SEARCH BAR //////////////////
+
+$("#search").keydown(function(tomato){
+	console.log("i'm a button!");
+	if(tomato.keyCode === 13) {
+		console.log("press enter to search");
+		let search = $("#search").val();
+		console.log("search", search);
+	}
+});
+
+
+
+
+
+
+/////////// FILTER EVENT LISTENERS ////////////
+$("#showUntrackedBtn").click(function (){
+	console.log("showUntrackedBtn",this);
+});
+
+$("#showUnwatchedBtn").click(function (){
+	console.log("showUnwatchedBtn",this);
+});
+
+$("#showWatchedBtn").click(function (){
+	console.log("showWatchedBtn",this);
+});
+
+$("#favoritesBtn").click(function (){
+	console.log("favoritesBtn",this);
+});
